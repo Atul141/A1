f3d9375ecd8b594b45ba592249291a5603f54041
Atul Kumar <atulbk@thoughtworks.com>
2016-07-05 21:17:36 +0530

    complete

---
+++ Triangletwo.java
@@ -0,0 +1,16 @@
+package A1;
+
+/**
+ * Created by Jayashree on 05-07-2016.
+ */
+public class Triangletwo {
+
+
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(args[0]);
+        for (int i = 0; i < num; i++)
+            System.out.print("*");
+
+    }
+}

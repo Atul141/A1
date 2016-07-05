8d6c4ade4d63f5f6d3fad07aef1665e0e42c4a05
Atul Kumar <atulbk@thoughtworks.com>
2016-07-05 21:25:58 +0530

    completed

---
+++ TriangleThree.java
@@ -0,0 +1,15 @@
+package A1;
+
+/**
+ * Created by Jayashree on 05-07-2016.
+ */
+public class TriangleThree {
+
+    public static void main(String[] args)
+        {
+            int num=Integer.parseInt(args[0]);
+            for(int i=0;i<num;i++)
+                System.out.println("*");
+
+        }
+}
